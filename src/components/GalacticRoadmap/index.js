5911769675d0d5c50dcb9ae5936b03a60df79212
Aniket <78921765+Aniket23079@users.noreply.github.com>
2026-06-26 19:07:56 +0530

    Delete src directory

--- src/components/GalacticRoadmap/index.js
+++
@@ -1,2 +0,0 @@
-export { default } from './GalacticRoadmap'
-export * from './RoadmapIcons'
--- src/components/ProfessionalInvolvements/index.js
+++
@@ -1,3 +0,0 @@
-export { default } from './ProfessionalInvolvements'
-export { default as ProfessionalCard } from './ProfessionalCard'
-export { default as ProfessionalModal } from './ProfessionalModal'
